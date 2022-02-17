a98aa45aa58ec0f4a10aa822df8bdf03f9986e79
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2022-02-17 10:22:48 +0200

    server init

---
+++ JWT-Authentication-Project/src/main.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from 'express'
+
+const PORT = 3000
+// create an instance server
+const app: Application = express()
+
+//git express
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Welcome',
+  })
+})
+
+// start express server
+app.listen(PORT, () => {
+  console.log(`Server is starting at prot:${PORT}`)
+})
