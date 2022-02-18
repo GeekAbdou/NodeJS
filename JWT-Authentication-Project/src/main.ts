365e0bbda2d10aa6e4b4f1a6e9851f5e17332fbc
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2022-02-18 08:12:02 +0200

    adding all middleawres

--- JWT-Authentication-Project/src/main.ts
+++ JWT-Authentication-Project/src/main.ts
@@ -1,20 +1,44 @@
 import express, { Application, Request, Response } from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 
 const PORT = 3000
 // create an instance server
 const app: Application = express()
+// Middleware to parses incoming requests with JSON payloads and is based on body-parser.
+app.use(express.json())
+// HTTP request logger middleware
+app.use(morgan('common'))
+// HTTP security middleware headers
+app.use(helmet())
+// Basic rate-limiting middleware for Express
+// Apply the rate limiting middleware to all requests
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: 'you request is more 100 .. try after one hour',
+  })
+)
 
-//git express
-
+// add routing for / path
 app.get('/', (req: Request, res: Response) => {
   res.json({
-    message: 'Welcome',
+    message: 'Hello World 🌍',
+  })
+})
+
+app.use((_: Request, res: Response) => {
+  res.status(404).json({
+    message: '404 Not found',
   })
 })
 
 // start express server
 app.listen(PORT, () => {
-  console.log(`Server is starting at prot:${PORT}`)
+  console.log(`Server port:${PORT}`)
 })
-
 export default app
--- new-project-init/src/__tests__/indexSpec.ts
+++ new-project-init/src/__tests__/indexSpec.ts
@@ -1,1 +1,11 @@
 import supertest from 'supertest'
+import app from '../main'
+
+const request = supertest(app)
+
+describe('Test basic endpoint server', () => {
+  it('Get the / endpoint', async () => {
+    const response = await request.get('/')
+    expect(response.status).toBe(200)
+  })
+})
