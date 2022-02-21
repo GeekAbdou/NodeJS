063810cbff8bc50baa4511a5750e05c67c41deeb
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2022-02-21 22:44:36 +0200

    move

--- img-Processing-api/src/config.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-console.log(process.env)
-
-const { PORT } = process.env
-
-export default {
-  port: PORT,
-}
--- img-Processing-api/src/interfaces/error.ts
+++
@@ -1,8 +0,0 @@
-interface Error {
-  name?: string
-  message?: string
-  stack?: string
-  status?: number
-}
-
-export default Error
